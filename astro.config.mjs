153f92b9251b1743e5c4373dd9fd47e7b403243f
会做饭的二哈 <baikaiwen12@outlook.com>
2025-02-26 19:53:04 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://acg-n.cn/",
+  site: "https://ldoubil.githib.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
